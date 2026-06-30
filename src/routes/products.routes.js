fda952abdbc6e25dbd4b0fbd62671fdfa5b64bb4
ashmitashrestha84 <ashmitashrestha247@gmail.com>
2026-06-30 16:32:52 +0545

    add:applied routing middleware

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -2,8 +2,13 @@ import express from "express";
 const router= express.Router();
 
 import { getAll,getbyID,create,update,remove } from "../controllers/products.controller.js";
+
+const mid=(req,res,next)=>{
+    console.log("get all products");
+    next();
+}
 //! CRUD PRODUCTS
-router.get("/", getAll);
+router.get("/", mid, mid, getAll);
 
 router.get("/:id",getbyID);
 router.post("/", create);
