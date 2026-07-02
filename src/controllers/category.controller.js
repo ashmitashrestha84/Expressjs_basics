1715c7733f2bc0b492c91e1bd664e3179ce872a8
ashmitashrestha84 <ashmitashrestha247@gmail.com>
2026-07-02 13:04:37 +0545

    add:database connection of category

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -1,33 +1,67 @@
+import mongoose from "mongoose";
 const category=[];
+const categorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      minLength: 3,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
-//getALL
-export const getAll=((req,res)=>{
+const Category = mongoose.model("Category", categorySchema);
+
+// Get all categories
+export const getAll = async (req, res, next) => {
+  try {
+    const categories = await Category.find();
+
     res.status(200).json({
-        message:"Category found",
-        success:true,
-        data:category,
-    })
-});
+      message: "Categories fetched successfully",
+      success: true,
+      data: categories,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
 
-export const getbyID=((req,res,next)=>{
-    const {id}=req.params
-    const categ=category.find((categ)=>categ.id===Number(id));
-    if(!categ){
-        next({
-        message:"Category not found",
-        statusCode:404,
-        })
+// Get category by ID
+export const getbyID = async(req, res, next) => {
+  try {
+    const {id} = req.params;
+    const categ = await findOne({id:id});
+
+    if (!categ) {
+      return next({
+        message: "Category not found",
+        statusCode: 404,
+      });
     }
     res.status(200).json({
-        message:"Category found",
-        success:true,
-        data:categ,
-    })
-});
+      message: "Category found",
+      success: true,
+      data: categ,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
 
-export const create=((req,res)=>{
-    const {name,price,brand}= req.body;
-    category.push({
+export const create=(req,res,next)=>{
+ try{
+   const {name,price,brand}= req.body;
+    const category=({
         name,
         price,
         brand,
@@ -37,50 +71,67 @@ export const create=((req,res)=>{
     res.status(200).json({
         message:"Product Created",
         success:true,
-        data:category[category.length-1],
+        data:category,
     })
-})
-
-export const update=((req,res,next)=>{
+}
+catch(err){
+  next(err);
+}
+}
+export const update=async(req,res,next)=>{
+  try{
     const {id}= req.params;
-    const index=category.findIndex((categ)=> categ.id=== Number(id))
     const {name,price,brand}=req.body;
+    const category=await Category.findByIdAndUpdate(
+      id,
+      {
+        name,
+        price,
+        brand,
+      },
+      {
+      new: true,
+      runValidators: true,
+      } 
+    )
     if(index===-1){
         next({
             message:"Category update fail",
             statusCode:404,
         })
     }
-        category[index]={
-        ...category,
-        name,
-        price,
-        brand,
-    }
     res.status(200).json({
         message:"Update success",
         success:true,
-        data:category[index],
+        data:category,
     })
-})
+}
+catch(err){
+  next(err);
+}
+}
 
-export const remove=(req, res,next) => {
-    const {id}=req.params;
+export const remove=async(req, res,next) => {
+    try{
+      const {id}=req.params;
 //   res.send("<h1>Product deleted</h1>");
-    const index=category.findIndex((categ)=>categ.id===Number(id));
-    if(index === -1){
+    const category=await Category.findByIdAndDelete(id);
+    if(!category){
         next({
             message:"Category delete failed",
             statusCode:404,
         });
         return;
-    } 
-    category.splice(index,1);
+    }
+    // category.splice(index,1);
     res.status(200).json({
         message:"Category deleted",
-        success:"true",
-        data:null,
+        success:true,
+        data:category,
     })
+}
+catch(err){
+   next(err);
+}
 };
-
 
